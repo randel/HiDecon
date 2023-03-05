b479d356b1c2448c2f402308fd30cb8cb3bfa1eb
Penghui Huang <80626712+Penghuihuang2000@users.noreply.github.com>
2023-03-05 16:33:30 -0500

    submit

---
+++ R/Data.R
@@ -0,0 +1,47 @@
+#' B
+#'
+#' @description The cell type mapping matrix used in the HiDecon example data.
+#'
+#'
+#' @name B
+#' @docType data
+"B"
+
+
+#' bulk.dat
+#'
+#' @description Simulated bulk tissue data of 126 samples. It's part of the simulated data used in HiDecon.
+#'
+#'
+#' @name bulk.dat
+#' @docType data
+"bulk.dat"
+
+
+#' ref.dat
+#'
+#' @description The single cell reference used in the HiDecon example data. It's part of the simulated data used in HiDecon.
+#'
+#'
+#' @name ref.dat
+#' @docType data
+"ref.dat"
+
+
+#' order.type
+#'
+#' @description Cell type order of the example data bottome layer of the hierarchical tree.
+#'
+#'
+#' @name order.type
+#' @docType data
+"order.type"
+
+#' ref.type
+#'
+#' @description Cell type labels of the single cell data ref.dat.
+#'
+#'
+#' @name ref.type
+#' @docType data
+"ref.type"
